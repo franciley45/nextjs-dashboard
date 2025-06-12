d46aafac9fcd53327a01c754a7ba5240f0b14beb
franciley <franciley.nascimento@radixeng.com>
2025-06-12 17:05:27 -0300

    ...

--- app/dashboard/customers/[id]/edit/page.tsx
+++ app/dashboard/customers/[id]/edit/page.tsx
@@ -3,7 +3,8 @@ import { fetchCustomers } from '@/app/lib/data';
 import Form from '@/app/ui/customers/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const customers = await fetchCustomers();
   const customer = customers.find((c) => c.id === params.id);
   if (!customer) notFound();
